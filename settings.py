82c1d6e39f38005d3fdb5dd8c37b1355f5d40ae8
Anatoly Gvozdev <anatoly@MacBook-Air.local>
2023-12-27 01:04:42 +0300

    Add tests

--- settings.py
+++ settings.py
@@ -6,3 +6,8 @@ REAL_DATABASE_URL = env.str(
     'REAL_DATABASE_URL',
     default="postgresql+asyncpg://admin:root@0.0.0.0:5438/postgres"
 )
+
+TEST_DATABASE_URL = env.str(
+    'TEST_DATABASE_URL',
+    default="postgresql+asyncpg://postgres_test:postgres_test@0.0.0.0:5439/postgres_test"
+)
